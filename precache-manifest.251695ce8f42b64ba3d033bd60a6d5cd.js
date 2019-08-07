48c98f09c9e64425ccacf75ddf89f99f4dd81101
Andy Nebs <andynebs@gmail.com>
2019-08-07 06:53:14 -0400

    Updates

---
+++ precache-manifest.251695ce8f42b64ba3d033bd60a6d5cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83aae14f1a373e8a72cd67443cca27ad",
+    "url": "/shirtsApp/index.html"
+  },
+  {
+    "revision": "606486028c3fc414ccd4",
+    "url": "/shirtsApp/static/css/2.52e666f5.chunk.css"
+  },
+  {
+    "revision": "de28a94131c5df4c3384",
+    "url": "/shirtsApp/static/css/main.da4b059a.chunk.css"
+  },
+  {
+    "revision": "606486028c3fc414ccd4",
+    "url": "/shirtsApp/static/js/2.adf01626.chunk.js"
+  },
+  {
+    "revision": "de28a94131c5df4c3384",
+    "url": "/shirtsApp/static/js/main.653e4a0c.chunk.js"
+  },
+  {
+    "revision": "517db55ba130431274a8",
+    "url": "/shirtsApp/static/js/runtime~main.694e52c4.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shirtsApp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shirtsApp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shirtsApp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shirtsApp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shirtsApp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
